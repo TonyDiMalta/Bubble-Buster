4200ef7afe7edca9df16b95bd3aba2c20d8074ed
Tony Di Malta <Sky-Angels8@hotmail.fr>
2019-02-26 10:59:11 +0100

    Manages the hex grid, it will be useful to place the bubbles on the screen and manage their neighbors.

---
+++ Assets/Scripts/Hex/HexCoordinates.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public struct HexCoordinates {
+
+	[SerializeField]
+	private int x, y;
+
+	public int X {
+		get {
+			return x;
+		}
+	}
+
+	public int Y {
+		get {
+			return y;
+		}
+	}
+
+	public HexCoordinates (int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public static HexCoordinates FromOffsetCoordinates (int x, int y) {
+		return new HexCoordinates(x, y);
+	}
+
+	public static HexCoordinates FromPosition (Vector3 position) {
+		float x = position.x / (HexMetrics.innerRadius * 2f);
+		float z = -x;
+
+		float offset = position.y / (HexMetrics.outerRadius * 1.5f);
+		z -= offset;
+
+		int iX = Mathf.RoundToInt(x);
+		int iY = Mathf.RoundToInt(-x -z);
+		int iZ = Mathf.RoundToInt(z);
+
+		if (iX + iY + iZ != 0) {
+			float dX = Mathf.Abs(x - iX);
+			float dY = Mathf.Abs(-x -z - iY);
+			float dZ = Mathf.Abs(z - iZ);
+
+			if (dX > dY && dX > dZ) {
+				iX = -iY - iZ;
+			}
+			else if (dY > dZ) {
+				iY = -iX - iZ;
+			}
+		}
+
+		return new HexCoordinates(iX, iY);
+	}
+
+	public override string ToString () {
+		return "(" + X.ToString() + ", " + Y.ToString() + ")";
+	}
+
+	public string ToStringOnSeparateLines () {
+		return X.ToString() + "\n" + Y.ToString();
+	}
+}
---
+++ Assets/Scripts/Hex/HexGrid.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HexGrid : MonoBehaviour {
+
+    [SerializeField]
+    private int width = 6, height = 6;
+
+	public Color defaultColor = Color.white;
+
+	public HexCell cellPrefab;
+	public Text cellLabelPrefab;
+
+	HexCell[] cells;
+
+	Canvas gridCanvas;
+	HexMesh hexMesh;
+
+	void Awake () {
+		gridCanvas = GetComponentInChildren<Canvas>();
+		hexMesh = GetComponentInChildren<HexMesh>();
+
+		cells = new HexCell[height * width];
+
+		for (int y = 0, i = 0; y < height; ++y) {
+			for (int x = 0; x < width; ++x) {
+				CreateCell(x, y, i++);
+			}
+		}
+	}
+
+	void Start () {
+		hexMesh.Triangulate(cells);
+	}
+
+	public void ColorCell (Vector3 position, Color color) {
+		position = transform.InverseTransformPoint(position);
+		HexCoordinates coordinates = HexCoordinates.FromPosition(position);
+        if (coordinates.X >= 0 && coordinates.X < width &&
+            coordinates.Y >= 0 && coordinates.Y < height)
+        {
+            int index = coordinates.X + coordinates.Y * width;
+            HexCell cell = cells[index];
+            cell.color = color;
+            hexMesh.Triangulate(cells);
+        }
+	}
+
+	void CreateCell (int x, int y, int i) {
+		Vector3 position;
+		position.x = (x + y * 0.5f - y / 2) * (HexMetrics.innerRadius * 2f);
+		position.y = y * (HexMetrics.outerRadius * 1.5f);
+		position.z = 0f;
+
+        HexCell cell = cells[i] = Instantiate<HexCell>(cellPrefab);
+		cell.transform.SetParent(transform, false);
+		cell.transform.localPosition = position;
+		cell.coordinates = HexCoordinates.FromOffsetCoordinates(x, y);
+		cell.color = defaultColor;
+
+		Text label = Instantiate<Text>(cellLabelPrefab);
+		label.rectTransform.SetParent(gridCanvas.transform, false);
+		label.rectTransform.anchoredPosition =
+			new Vector2(position.x, position.y);
+		label.text = cell.coordinates.ToStringOnSeparateLines();
+	}
+}
